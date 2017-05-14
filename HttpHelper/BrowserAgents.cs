cb48e4a067e8109d427e77eb19c3b88dca0e78ec
Ajit <codemasterblackperl@outlook.com>
2017-05-14 22:19:12 +0530

    adding browser agent

---
+++ HttpHelper/BrowserAgents.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpHelper
+{
+    public enum BrowserAgents
+    {
+        [Description("Mozilla / 5.0(Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1667.0 Safari/537.36")]
+        Chrome_Windows,
+        [Description("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36")]
+        Chrome_Mac,
+        [Description("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")]
+        Firefox_Windows,
+        [Description("Mozilla/5.0 (X11; Linux x86_64; rv:28.0) Gecko/20100101 Firefox/28.0")]
+        Firefox_Linux,
+    }
+}
